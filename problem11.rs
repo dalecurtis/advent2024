dbd89a8c1bd414b79cc891bace0ec37a18d63da4
Dale Curtis <dalecurtis@chromium.org>
2024-12-11 17:28:33 +0000

    Add problem 11 code.
    Notes on the solve:
    - Initial thought was "this won't fit in memory", but surely at
    least the last row will. 2^25 * 8 definitely fits. So I wrote a
    now discarded in place solution for part1.
    - Then on part 2, ah even that won't fit, so before we trek into
    dynamic programming, maybe there's a pure math solution...
    - ... and not smart enough to find one or it doesn't exist, so
    just use look up table and brute force it.

---
+++ problem11.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+
+// Turns 1234 -> 12, 34, etc.
+fn split_number(v: u64, num_digits: u32) -> (u64, u64) {
+    let divisor = 10_u64.pow(num_digits / 2) as u64;
+    let first_part: u64 = v / divisor;
+    let second_part: u64 = v % divisor;
+    return (first_part, second_part);
+}
+
+// Splits stones according to the rules. Sticks result in `cache`
+fn split_stone(v: u64, d: u8, max_d: u8, mut cache: &mut HashMap<u8, HashMap<u64, u64>>) -> u64 {
+    if d >= max_d {
+        return 1;
+    }
+
+    // Can't hold a cache entry while we pass it on below, so a scope block is used.
+    {
+        let vc = cache.entry(d).or_insert(HashMap::new());
+        let sc = vc.get(&v);
+        if !sc.is_none() {
+            return *sc.unwrap();
+        }
+    }
+
+    let result: u64;
+    if v == 0 {
+        result = split_stone(1, d + 1, max_d, &mut cache);
+    } else {
+        let num_digits = v.ilog10() + 1;
+        if num_digits & 1 == 0 {
+            let (first_four, last_four) = split_number(v, num_digits);
+            result = split_stone(first_four, d + 1, max_d, &mut cache)
+                + split_stone(last_four, d + 1, max_d, &mut cache);
+        } else {
+            result = split_stone(v * 2024, d + 1, max_d, &mut cache);
+        }
+    }
+
+    cache.get_mut(&d).expect("fail").insert(v, result);
+    return result;
+}
+
+fn main() {
+    const BLINKS: u8 = 75; // 25 for part 1.
+
+    // let stones: Vec<u64> = vec![125, 17];
+    let stones: Vec<u64> = vec![5688, 62084, 2, 3248809, 179, 79, 0, 172169];
+
+    for v in &stones {
+        print!("{} ", v)
+    }
+    println!();
+
+    let mut stone_count = 0;
+    let mut cache = HashMap::new();
+    for v in stones {
+        stone_count += split_stone(v, 0, BLINKS, &mut cache);
+        println!("partial_count={}", stone_count);
+    }
+    let mut cache_size = 0;
+    for (_, v) in cache {
+        cache_size += v.len();
+    }
+
+    println!("stones={}, cache_size={}", stone_count, cache_size);
+}
